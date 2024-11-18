b7d28aeb0bdd76a7c14b0157e0edf8cf73a68f41
rollandscream <104349871+rollandscream@users.noreply.github.com>
2024-11-18 15:54:47 +0100

    added basic functionality

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const app = express();
 var mongoose = require('mongoose');
 const { json } = require('body-parser');
 mongoose.connect(process.env.MONGO_URI);
+var validator = require('validator');
+const bodyParser = require('body-parser');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -28,8 +30,45 @@ const Schema = mongoose.Schema;
 
 let urlSchema = new Schema({
   longUrl: String,
-  shortUrl: String
+  shortUrl: Number
+});
+
+let urlShortened = mongoose.model('urlShortener', urlSchema);
+let shortUrlInt = 0;
+
+app.use("/api/shorturl", bodyParser.urlencoded({extended: false}));
+//get long url from the post
+app.post("/api/shorturl", function (req, res) {
+  //check if it's format is ok
+  console.log(req.body.url);
+  if (!validator.isURL(req.body.url)) {
+    //generate error response
+    res.json({"error": "invalid url"});
+  } else {
+    //generate a number for a short url
+    shortUrlInt++;
+    //save to database
+    var shortUrl = new urlShortened({longUrl: req.body.url, shortUrl: shortUrlInt});
+    shortUrl.save()
+            .then((doc) => console.log(doc))
+            .catch((err) => console.error(err));
+    res.json({"original_url": req.body.url, "short_url": shortUrlInt});
+  }
 })
+
+//read short url from the query
+app.get("/api/shorturl/:shorturl", function (req, res) {
+  console.log(req.params.shorturl);
+  var urlToFind = 1; //parseInt(req.params.shorturl);
+  //find it in the database
+  urlShortened.find({shortUrl: urlToFind})
+              .then((doc) => {
+                res.redirect(doc[0]._doc.longUrl);
+              })
+              .catch((err) => console.log(err));
+})
+
+//redirect to the long url
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
